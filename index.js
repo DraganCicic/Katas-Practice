08f503145387f2e17c329fe506f109ed1b4e6a14
Dragan Cicic <your3mail>
2021-08-29 15:12:51 -0400

    sunday practice

--- index.js
+++ index.js
@@ -18,3 +18,30 @@ function solution(a, b) {
         return b + a + b;
     }
 }
+
+
+// Sum of positive
+// You get an array of numbers, return the sum of all of the positives ones.
+
+function positiveSum(arr) {
+    var total = 0;    
+    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+      if (arr[i] > 0) {                   // if arr[i] is greater than zero
+        total += arr[i];                  // add arr[i] to total
+      }
+    }
+    return total;                         // return total
+  }
+
+
+//   Triple Trouble
+//   Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+
+  function tripleTrouble(one, two, three){
+    let result = "";
+    for ( let i=0 ; i < one.length; i++){
+      result += one[i] + two[i] + three[i];
+   }
+  return result;
+  }
