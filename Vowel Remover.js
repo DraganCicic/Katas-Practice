65742439c6117fdaa6362b41684377d2587792b1
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-21 09:26:40 -0400

    Update Vowel Remover.js

--- Vowel Remover.js
+++ Vowel Remover.js
@@ -16,3 +16,10 @@ function shortcut(string){
   }
   return output;
 }
+
+
+function shortcut(str) {
+  return str.split('').filter(function(e) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+  }).join('')
+}
