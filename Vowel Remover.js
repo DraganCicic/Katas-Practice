5cceddbda7e7cac7f57d7fac25b0acc752fb9cd6
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-21 09:26:09 -0400

    Update Vowel Remover.js

--- Vowel Remover.js
+++ Vowel Remover.js
@@ -4,3 +4,15 @@
 function shortcut(string){
     return string.replace(/[aeiou]/g,'')
 }
+
+
+function shortcut(string){
+  var vowels = "aeiou";
+  var output = "";
+  for (var i = 0; i < string.length; i++) {
+    if (!(vowels.indexOf(string[i]) > -1)) {
+      output += string[i];
+    }
+  }
+  return output;
+}
