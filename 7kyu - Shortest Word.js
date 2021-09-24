7c945010b36cd91a90b8190f5d565e3fd3600894
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-24 09:32:29 -0400

    Update 7kyu - Shortest Word.js

--- 7kyu - Shortest Word.js
+++ 7kyu - Shortest Word.js
@@ -9,3 +9,15 @@ function findShort(s){
   return result
   
 }
+// ----------------------------------------------------------------
+
+function findShort(s){
+  var arr = s.split(' ');
+  var smallest = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].length < smallest.length){
+      smallest = arr[i];
+    }
+  }
+  return smallest.length;
+}
