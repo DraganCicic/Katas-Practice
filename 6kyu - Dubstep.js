45958b8107f8447b8a6eb2404526830c0def9774
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-24 09:54:03 -0400

    Update 6kyu - Dubstep.js

--- 6kyu - Dubstep.js
+++ 6kyu - Dubstep.js
@@ -21,3 +21,21 @@
 function songDecoder(song){
   return  song.replace(/(WUB)+/gi,' ').trim()
 }
+// --------------------------------------------------------
+
+function songDecoder(song){
+  var str = '';
+  for(var i=0;i<song.length;i++){
+    if((song[i] == 'W' && song[i+1] == 'U' && song[i+2] == 'B') 
+    || (song[i-1] == 'W' && song[i] == 'U' && song[i+1] == 'B')
+    || (song[i-2] == 'W' && song[i-1] == 'U' && song[i] == 'B'))
+      str += str[str.length-1] !== ' ' ? ' ' : '';
+    else
+      str += song[i];
+  }
+  return str.trim();
+}
+
+// --------------------------------------------------------
+
+
