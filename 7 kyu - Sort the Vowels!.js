5ff071d9efecfba692000c5811b4f0a6e2f28315
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-29 10:12:28 -0400

    Update 7 kyu - Sort the Vowels!.js

--- 7 kyu - Sort the Vowels!.js
+++ 7 kyu - Sort the Vowels!.js
@@ -53,4 +53,10 @@ function sortVowels(s){
 }
 
 //--------------------------------------------------
-
+function sortVowels(s){
+  return typeof s === 'string' 
+          ? s.split('')
+             .map(el => /[aeuio]/gi.test(el) ? `|${el}` : `${el}|`)
+             .join('\n')
+          : '';
+}
