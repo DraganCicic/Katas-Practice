b590158a62b82a2862e14b3f265b08fe37ab43dd
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-29 10:11:18 -0400

    Update 7 kyu - Sort the Vowels!.js

--- 7 kyu - Sort the Vowels!.js
+++ 7 kyu - Sort the Vowels!.js
@@ -32,3 +32,10 @@ function sortVowels(s){
 }
 
 //--------------------------------------------------
+
+
+function sortVowels(s){
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  if (typeof s === "string") return [...s].map(letter => vowels.includes(letter) ? `|${letter}` : `${letter}|`).join('\n');
+  return '';
+}
