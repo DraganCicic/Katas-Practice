3dbb977d50972e987b3ffb45350c43cdd598ad21
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-09-30 09:20:50 -0400

    Update Enumerable Magic #1 - True for All?.js

--- Enumerable Magic #1 - True for All?.js
+++ Enumerable Magic #1 - True for All?.js
@@ -14,3 +14,15 @@ function all( arr, fun ){
 
 
 // ---------------------------------
+function all(arr, fun)
+{
+  var length = arr.length;
+  
+  for (var i=0; i<length; ++i)
+    if (!fun(arr[i]))
+      return false;
+  
+  return true;
+}
+
+// ------------------------------
