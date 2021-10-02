e31290507685716cf53546a5f02a54ee54ea0161
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-02 08:51:17 -0400

    Update Simple multiplication.js

--- Simple multiplication.js
+++ Simple multiplication.js
@@ -3,3 +3,13 @@
 function simpleMultiplication(n){
   return n % 2 == 0 ? n * 8 : n * 9
 }
+// ---------------------------------
+function simpleMultiplication(value){
+    
+    if(value%2===0 ){
+     return value *8
+    }
+    else{
+    return value* 9;
+    }    
+}
