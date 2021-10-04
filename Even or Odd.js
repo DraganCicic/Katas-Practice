3ef5485d652cf9ea66d23574b339d4e0478e768b
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-03 21:48:53 -0400

    Update Even or Odd.js

--- Even or Odd.js
+++ Even or Odd.js
@@ -15,3 +15,9 @@ function even_or_odd(number) {
 }
 
 // ----------------------------
+
+function even_or_odd(number) {
+   return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+// --------------------------
