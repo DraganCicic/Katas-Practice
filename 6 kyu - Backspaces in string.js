6a9e60be2ce6cf2b9ca68f69a4b0faa0caa81ba8
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-04 10:33:45 -0400

    Update 6 kyu - Backspaces in string.js

--- 6 kyu - Backspaces in string.js
+++ 6 kyu - Backspaces in string.js
@@ -32,6 +32,22 @@ function cleanString(s) {
   return result.join("");
 }
 
+// ------------------------------
+
+function cleanString(s) {
+
+  let result = ''
+  for(let i=0; i<s.length;i++){
+    //taking care of the letters of the string
+    if(s[i] !== "#"){
+      result += s[i]
+    } else {
+      //remove the last letter of result
+      result = result.slice(0, -1)
+    }
+  }
+  return result
+};
 
 // -------------------------------
 
