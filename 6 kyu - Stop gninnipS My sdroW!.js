90431c58c18668d116dc859a2b31f6e6dd45885f
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-05 09:51:15 -0400

    Update 6 kyu - Stop gninnipS My sdroW!.js

--- 6 kyu - Stop gninnipS My sdroW!.js
+++ 6 kyu - Stop gninnipS My sdroW!.js
@@ -28,3 +28,9 @@ function spinWords(words){
 }
 
 // -----------------------------
+
+function spinWords(string){
+  return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+}
+
+// -----------------------------
