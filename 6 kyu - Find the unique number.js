e90445a5a908d2c8a0ce29e50a5fa48362716a0d
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-06 09:16:28 -0400

    Update 6 kyu - Find the unique number.js

--- 6 kyu - Find the unique number.js
+++ 6 kyu - Find the unique number.js
@@ -11,3 +11,10 @@ function findUniq(arr) {
 }
 
 // ----------------------------------------
+
+function findUniq(arr) {
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+}
+
+// ---------------------------------------
