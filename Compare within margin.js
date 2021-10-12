356ca64920f3fec6292f1220d4803734afaf8e98
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-12 09:42:26 -0400

    Update Compare within margin.js

--- Compare within margin.js
+++ Compare within margin.js
@@ -24,3 +24,12 @@ function closeCompare(a, b, margin=0){
   if (a < b) return -1
   return 1
 }
+
+// ---------------------------------
+
+
+function closeCompare(a, b, m = 0){
+  return Math.abs(a - b) <= m? 0: Math.sign(a - b);
+}
+
+// --------------------------------
