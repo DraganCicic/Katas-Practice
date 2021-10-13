efd6d754c0e7911c36baad251e32967ff3477064
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-13 11:06:20 -0400

    Update Function 2 - squaring an argument.js

--- Function 2 - squaring an argument.js
+++ Function 2 - squaring an argument.js
@@ -1,3 +1,9 @@
 // Now you have to write a function that takes an argument and returns the square of it.
 
 const square=(a)=> Math.pow(a,2);
+
+// ----------------------
+
+let square = function(a){
+  return a*a;
+}
