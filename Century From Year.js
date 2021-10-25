2c20dd69da172e1015e7dcf6310491ee6e96cd83
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-25 16:51:52 +0200

    Update Century From Year.js

--- Century From Year.js
+++ Century From Year.js
@@ -11,3 +11,11 @@
 
 
 const century = year => Math.ceil(year/100)
+
+
+// -------------------------
+
+
+function century(year) {
+  return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
+}
