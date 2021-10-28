6daaba949bb1929da81ca6c2148c5ef25e18e573
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-28 09:35:57 -0400

    Update Counting sheep...js

--- Counting sheep...js
+++ Counting sheep...js
@@ -15,3 +15,16 @@
 function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(Boolean).length;
 }
+// ------------------------------------
+
+
+function countSheeps(arrayOfSheep) {
+  
+  let num = 0;
+  
+  for(let i = 0; i < arrayOfSheep.length; i++)
+    if(arrayOfSheep[i] == true)
+      num++;
+      
+  return num;
+}
