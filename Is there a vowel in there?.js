def702448fe3c061f3674f2ca795ead175854bd7
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-28 10:27:06 -0400

    Update Is there a vowel in there?.js

--- Is there a vowel in there?.js
+++ Is there a vowel in there?.js
@@ -1,3 +1,14 @@
+
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// Return the resulting array.
+
+
+
+
+
 function isVow(a){
 return a.map(i => {
 if (String.fromCharCode(i) === 'a')
@@ -14,3 +25,6 @@ else
 return i;
 })
 }
+
+
+// ----------------------------
