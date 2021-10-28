c319a10fb9a3e5d7d733648aff6b0e7822f2901d
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-28 10:28:36 -0400

    Update Is there a vowel in there?.js

--- Is there a vowel in there?.js
+++ Is there a vowel in there?.js
@@ -28,3 +28,14 @@ return i;
 
 
 // ----------------------------
+
+
+function isVow(a){
+  let vowels=['a','e','i','o','u'];
+  return a.map((el=>String.fromCharCode(el))).map((el)=>{
+  if(!vowels.includes(el)){
+    el=el.charCodeAt()
+  }
+  return el
+})
+}
