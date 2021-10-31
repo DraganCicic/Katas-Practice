cb04c2c432d57bce77c688e8ac1de3005d2ba93f
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-10-31 16:12:38 -0400

    Update What's the real floor?.js

--- What's the real floor?.js
+++ What's the real floor?.js
@@ -16,3 +16,10 @@
 function getRealFloor(n) {
   return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
 }
+// ----------------------------------------
+
+const getRealFloor = n => {
+  if(n >= 13) return n - 2
+  if(n > 0) return n - 1
+  return n
+}
