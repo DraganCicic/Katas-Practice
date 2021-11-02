05f5b1ea27fbc1b83a649f9960a1302cc1f7a330
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-11-02 12:57:29 -0400

    Update Are You Playing Banjo?.js

--- Are You Playing Banjo?.js
+++ Are You Playing Banjo?.js
@@ -22,3 +22,11 @@ function areYouPlayingBanjo(name) {
     return name + ' does not play banjo';
   }
 }
+
+function areYouPlayingBanjo(name) {
+  if( name[0] == "R" || name[0] == "r"){
+    return `${name} plays banjo`
+  }else{
+    return `${name} does not play banjo`
+  }
+}
