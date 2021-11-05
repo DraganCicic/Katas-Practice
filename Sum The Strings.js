8c250296dc47c1f98f4ae62ad495d85e3b7c71de
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-11-05 12:46:11 -0400

    Update Sum The Strings.js

--- Sum The Strings.js
+++ Sum The Strings.js
@@ -16,3 +16,11 @@
 function sumStr(a,b) {
   return String(Number(a) + Number(b))
 }
+// -----------------------------
+
+
+function sumStr(a,b) {
+  const num1 = Number(a);
+  const num2 = Number(b);
+  return String(num1 + num2)
+}
