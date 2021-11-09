f4cff9ee6dc9480d23b2b116bb98658ebc95d910
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-11-09 11:06:57 -0500

    Update Filter out the geese.js

--- Filter out the geese.js
+++ Filter out the geese.js
@@ -17,3 +17,19 @@ function gooseFilter (birds) {
   
   return birds.filter(b => !geese.includes(b));
   }
+
+// -------------------------------
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+var newArray = [];
+  for (var i =0;i<birds.length;i++) {
+  if (!geese.includes(birds[i])) {
+    newArray.push(birds[i]);
+  }
+
+  
+  }
+return newArray;
+};
