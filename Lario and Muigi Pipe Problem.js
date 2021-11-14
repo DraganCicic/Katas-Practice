e549a68fb9e319e3a00748bddb057d011831c485
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-11-14 14:40:06 -0500

    Update Lario and Muigi Pipe Problem.js

--- Lario and Muigi Pipe Problem.js
+++ Lario and Muigi Pipe Problem.js
@@ -17,3 +17,18 @@ function pipeFix(numbers){
   for (let i=Math.min.apply(null,numbers); i<=q;i++) result.push(i)
 return result
 }
+
+// ------------------------------------
+
+function pipeFix(numbers){
+  var min = numbers[0];
+  var max = numbers[numbers.length - 1];
+  var array = [];
+  
+  for(var i = min; i<=max; i++)
+  {
+    array.push(i);
+  }
+  
+  return array;
+}
