58467c2d2138f3773b3a9ca205c041117cb781a3
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-11-27 23:39:45 -0500

    Update Switch it Up!.js

--- Switch it Up!.js
+++ Switch it Up!.js
@@ -1,3 +1,12 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+
+
+
 function switchItUp(number){
   switch(number) {
     case 0:
@@ -24,3 +33,30 @@ function switchItUp(number){
       return 'Unknown number';
   }
 }
+
+// -------------------------
+
+function switchItUp(number){
+ if (number === 1){
+   return "One"
+ }else if ( number === 2){
+   return "Two"
+ }else if(number === 3){
+   return "Three"
+ }else if( number === 5){
+   return "Five"
+ }else if(number ===4 ){
+   return "Four"
+ }else if (number === 6){
+   return "Six"
+ }else if (number === 7){
+   return "Seven"
+ }else if(number === 8){
+   return "Eight"
+ }else if (number === 9){
+   return "Nine"
+ }else{
+   return "Zero"
+ }
+}
+
