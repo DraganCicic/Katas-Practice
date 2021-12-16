290a2e43940e9074dde69da6de0b57e082881c1e
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-12-16 16:13:39 -0500

    Update Student's Final Grade.js

--- Student's Final Grade.js
+++ Student's Final Grade.js
@@ -27,3 +27,22 @@ function finalGrade (exam, projects) {
   if(exam > 50 & projects >= 2) return 75;
   return 0;
 }
+// ------------------------------------
+
+finalGrade = (exam, projects) => {
+  switch (true) {
+       case (exam > 90 || projects > 10):
+       return 100
+       break;
+       case (exam > 75 && projects >= 5):
+       return 90;
+       break;
+       case (exam > 50 && projects >= 2):
+       return 75
+       break;
+       case (exam <= 55 || projects <= 2):
+       return 0;
+       break;
+       
+}
+}
