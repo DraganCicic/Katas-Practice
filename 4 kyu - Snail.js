f7c0fd7f391793fc9615e4e11472e047629f8027
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2021-12-19 19:45:59 -0500

    Update 4 kyu - Snail.js

--- 4 kyu - Snail.js
+++ 4 kyu - Snail.js
@@ -27,3 +27,22 @@ function snail(array) {
   }
   return vector;
 }
+// ------------------------------
+
+snail = function(array) {
+  var size = array.length;
+  
+  if (size == 0)
+    return [];
+
+  if (size == 1)
+    return array[0];
+  
+  var top    = array[0].slice(0, -1);
+  var right  = array.slice(0, -1).map(a => a[size - 1]);
+  var bottom = array[size -1].slice(1).reverse();
+  var left   = array.slice(1).map(a => a[0]).reverse();
+  var inner  = array.slice(1, -1).map(a => a.slice(1, -1));
+
+  return [].concat(top, right, bottom, left, snail(inner));
+}
