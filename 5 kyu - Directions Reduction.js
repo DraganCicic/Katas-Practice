c29dd45d2ad609e9a016fa716ffa7a99852ec9a0
DraganCicic <86571711+DraganCicic@users.noreply.github.com>
2022-01-17 22:30:56 -0500

    Update 5 kyu - Directions Reduction.js

--- 5 kyu - Directions Reduction.js
+++ 5 kyu - Directions Reduction.js
@@ -43,3 +43,17 @@ function dirReduc(arr) {
   while (pattern.test(str)) str = str.replace(pattern, "");
   return str.match(/(NORTH|SOUTH|EAST|WEST)/g) || [];
 }
+
+//----------------------------------------------------
+
+function dirReduc(plan) {
+  var opposite = {
+    'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+  return plan.reduce(function(dirs, dir){
+      if (dirs[dirs.length - 1] === opposite[dir])
+        dirs.pop();
+      else
+        dirs.push(dir);
+      return dirs;
+    }, []);
+}
